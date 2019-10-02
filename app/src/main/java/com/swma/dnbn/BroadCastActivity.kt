f4b7455d8b7293fb388e911fdbde6a582d14664d
BearHunter49 <38463645+BearHunter49@users.noreply.github.com>
2019-10-02 20:43:52 +0900

    (BroadCast) 방송 모듈 추가 레이아웃 일부 수정 레이아웃 애니메이션 추가(handler 이용)

---
+++ app/src/main/java/com/swma/dnbn/BroadCastActivity.kt
@@ -0,0 +1,185 @@
+package com.swma.dnbn
+
+import android.content.Context
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.Handler
+import android.view.MotionEvent
+import android.view.SurfaceHolder
+import android.view.View
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import com.pedro.encoder.input.video.CameraOpenException
+import com.pedro.rtplibrary.rtmp.RtmpCamera2
+import kotlinx.android.synthetic.main.activity_broad_cast.*
+import net.ossrs.rtmp.ConnectCheckerRtmp
+
+class BroadCastActivity : AppCompatActivity(), ConnectCheckerRtmp, SurfaceHolder.Callback {
+
+    private lateinit var rtmpCamera2: RtmpCamera2
+    private val streamUrl = "rtmp://13.125.40.80:1935/dnbn/Test"
+    private lateinit var animation: Animation
+    private var check = 0
+    lateinit var handler: Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        setContentView(R.layout.activity_broad_cast)
+
+        // Fade-out Animation
+        handler = Handler()
+        animation = AnimationUtils.loadAnimation(this, R.anim.fadeout)
+        animation.setAnimationListener(object: Animation.AnimationListener{
+            override fun onAnimationRepeat(p0: Animation?) {
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {
+                lytBroadCast.visibility = View.GONE
+            }
+
+            override fun onAnimationStart(p0: Animation?) {
+            }
+        })
+
+        // RTMP Instance 연결
+        rtmpCamera2 = RtmpCamera2(openglView, this)
+        openglView.holder.addCallback(this)
+
+        displayLayout()
+
+        // 방송 버튼
+        btn_broadcastStart.setOnClickListener {
+            if (!rtmpCamera2.isStreaming) {
+                if (rtmpCamera2.prepareAudio() and rtmpCamera2.prepareVideo()) {
+                    btn_broadcastStart.text = "방송끄기"
+                    rtmpCamera2.startStream(streamUrl)
+                } else {
+                    Toast.makeText(this, "다시 시도 해 주세요!", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                btn_broadcastStart.text = "촬영시작"
+                rtmpCamera2.stopStream()
+            }
+        }
+
+        // 화면 전환 버튼
+        btn_switch.setOnClickListener {
+            try {
+                rtmpCamera2.switchCamera()
+            } catch (e: CameraOpenException) {
+                e.printStackTrace()
+            }
+        }
+
+        // 나가기 버튼
+        btn_close.setOnClickListener {
+            if (rtmpCamera2.isStreaming) {
+                Toast.makeText(this, "방송을 먼저 종료 해 주세요!", Toast.LENGTH_SHORT).show()
+            } else {
+                AlertDialog.Builder(this).setTitle("경고!").setMessage("정말로 종료 하시겠습니까?")
+                    .setPositiveButton("확인"){ _, _ -> finish() }
+                    .setNegativeButton("취소", null)
+                    .show()
+            }
+        }
+
+    }
+
+    // 터치 이벤트
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (event?.action == MotionEvent.ACTION_DOWN){
+            // 레이아웃 떠 있을 시
+            if (check == 1){
+                handler.removeMessages(0)
+                lytBroadCast.visibility = View.GONE
+                check = 0
+            }
+            // 레이아웃 없을 시
+            else{
+                displayLayout()
+            }
+        }
+        return super.onTouchEvent(event)
+    }
+
+    // 레이아웃 관리
+    private fun displayLayout() {
+        if (check == 0){
+            check = 1
+            lytBroadCast.visibility = View.VISIBLE
+            handler.postDelayed({
+                lytBroadCast.startAnimation(animation)
+                check = 0
+            }, 4000)
+        }
+    }
+
+
+    override fun onBackPressed() {
+    }
+
+    // ----- 방송 인터페이스 -----
+    override fun onAuthSuccessRtmp() {
+    }
+
+    override fun onNewBitrateRtmp(bitrate: Long) {
+    }
+
+    // 채널 연결 성공 시
+    override fun onConnectionSuccessRtmp() {
+        runOnUiThread {
+            textCountDown.visibility = View.VISIBLE
+            val countDownTimer = object : CountDownTimer(3000, 1000) {
+
+                override fun onTick(p0: Long) {
+                    textCountDown.text = String.format("%d", p0 / 1000L)
+                }
+
+                override fun onFinish() {
+                    textCountDown.visibility = View.GONE
+                    Toast.makeText(this@BroadCastActivity, "방송을 시작합니다!", Toast.LENGTH_SHORT).show()
+                }
+            }
+            countDownTimer.start()
+        }
+    }
+
+    override fun onConnectionFailedRtmp(reason: String?) {
+        runOnUiThread {
+            Toast.makeText(this, "다시 시도 해 주세요!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onAuthErrorRtmp() {
+    }
+
+    override fun onDisconnectRtmp() {
+        runOnUiThread {
+            Toast.makeText(this, "방송 종료", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
+        rtmpCamera2.startPreview()
+    }
+
+    override fun surfaceDestroyed(p0: SurfaceHolder?) {
+        if (rtmpCamera2.isStreaming) {
+            rtmpCamera2.stopStream()
+        }
+        rtmpCamera2.stopPreview()
+    }
+
+    override fun surfaceCreated(p0: SurfaceHolder?) {
+    }
+
+
+}
--- app/src/main/java/com/swma/dnbn/fragment/UserShoppingFragment.kt
+++ app/src/main/java/com/swma/dnbn/fragment/UserShoppingFragment.kt
@@ -1,14 +1,23 @@
 package com.swma.dnbn.fragment
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.swma.dnbn.BroadCastActivity
 
 import com.swma.dnbn.R
+import kotlinx.android.synthetic.main.fragment_user_shopping.view.*
 
 class UserShoppingFragment : Fragment() {
+
+    private val PERMISSIONS = arrayOf(android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.CAMERA)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,8 +25,42 @@ class UserShoppingFragment : Fragment() {
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_user_shopping, container, false)
 
+        rootView.apply {
+            btn_broadcast.setOnClickListener {
+
+                // 영상 촬영 권한
+                if (!hasPermission(requireContext(), PERMISSIONS)) {
+                    ActivityCompat.requestPermissions(requireActivity(), PERMISSIONS, 49)
+                }
+                else{
+                    startActivity(Intent(requireContext(), BroadCastActivity::class.java))
+                }
+            }
+        }
 
         return rootView
+    }
+
+    // Permit 확인
+    private fun hasPermission(context: Context, permissions: Array<String>): Boolean {
+        for (permit in permissions) {
+            if (ActivityCompat.checkSelfPermission(context, permit) != PackageManager.PERMISSION_GRANTED)
+                return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == 49) {
+            // 권한 허용 시
+            if (grantResults.isNotEmpty() and (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                startActivity(Intent(requireContext(), BroadCastActivity::class.java))
+            }
+            // 권한 거부 시
+            else {
+                Toast.makeText(requireContext(), "모든 권한 허용이 필수입니다!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 }
