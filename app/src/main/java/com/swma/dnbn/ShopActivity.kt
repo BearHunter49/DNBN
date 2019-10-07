793528a31cc07799774e24956d7fa1aa292222ec
BearHunter49 <38463645+BearHunter49@users.noreply.github.com>
2019-10-07 18:31:12 +0900

    UserFragment 스크롤 매커니즘 변경 (AppBarLayout 이용)

---
+++ app/src/main/java/com/swma/dnbn/ShopActivity.kt
@@ -0,0 +1,17 @@
+package com.swma.dnbn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShopActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop)
+
+
+
+
+
+    }
+}
