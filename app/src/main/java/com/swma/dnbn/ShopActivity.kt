c067f51f80d099d5f3c422d762fe3a94483bbb20
BearHunter49 <38463645+BearHunter49@users.noreply.github.com>
2019-10-08 14:12:22 +0900

    ShopActivity Toolbar(뒤로가기 버튼) 추가 하단 탭 추가

--- app/src/main/java/com/swma/dnbn/ShopActivity.kt
+++ app/src/main/java/com/swma/dnbn/ShopActivity.kt
@@ -2,14 +2,25 @@ package com.swma.dnbn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.swma.dnbn.fragment.ShopFragment
-import kotlinx.android.synthetic.main.activity_shop.*
+import kotlinx.android.synthetic.main.row_shop_toolbar.*
 
 class ShopActivity : AppCompatActivity() {
+
+    override fun onStart() {
+        shop_toolbar_title.text = "상품 정보"
+        super.onStart()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop)
+
+        // Toolbar
+        setSupportActionBar(shop_toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         // Intent 정보 받아오기
 
@@ -17,4 +28,14 @@ class ShopActivity : AppCompatActivity() {
        supportFragmentManager.beginTransaction().add(R.id.shopContainer, ShopFragment(), "Shop").commit()
 
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
 }
