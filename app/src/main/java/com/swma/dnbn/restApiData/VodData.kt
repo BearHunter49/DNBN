99d3af63a175311f385dd699d3c26a24c0df6d41
서주원 <rkdcjf987@naver.com>
2019-10-28 18:41:00 +0900

    임시

--- app/src/main/java/com/swma/dnbn/restApiData/VodData.kt
+++ app/src/main/java/com/swma/dnbn/restApiData/VodData.kt
@@ -2,4 +2,5 @@ package com.swma.dnbn.restApiData
 
 data class VodData (val id: Int, val productId: Int, val name: String,
                     val uploaderId: Int, val url: String, val categoryId: Int,
-                    val description: String, val thumbnailUrl: String)
+                    val description: String, val thumbnailUrl: String,
+                    val uploadAt: String)
